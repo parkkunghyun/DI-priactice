54abfbe94fa777b33ea3137a2e17fb413892ce6a
박경현 <rudgus4620@gmail.com>
2023-03-08 13:40:53 +0900

    singleton bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,22 +10,33 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class AppConfig {
    // 각 메서드에Bean 을 작성!
+    // call memberService
+    // call memberRepository
+    // call memberRepository
+    // call orderService
+    // call memberRepository
+
+    // 실제로는 memberService memberRepository orderService 한번씩만나옴!
+    // spring이 어떻게든 싱글톤을 보장하려고 하는구나
+    //
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return  new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
